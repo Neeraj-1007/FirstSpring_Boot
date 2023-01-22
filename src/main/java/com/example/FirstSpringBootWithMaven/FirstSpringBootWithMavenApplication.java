ba23a28bb323c49a08f94b9eb5109aa7a31e8770
Neeraj-1007 <neerajkumarjha10@gmail.com>
2023-01-22 19:32:51 +0530

    First Commit

---
+++ src/main/java/com/example/FirstSpringBootWithMaven/FirstSpringBootWithMavenApplication.java
@@ -0,0 +1,13 @@
+package com.example.FirstSpringBootWithMaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringBootWithMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringBootWithMavenApplication.class, args);
+	}
+
+}
