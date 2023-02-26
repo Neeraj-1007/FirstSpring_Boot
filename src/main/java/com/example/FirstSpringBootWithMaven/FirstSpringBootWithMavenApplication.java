ef1126ea0075a66c8b8980bfd9e1ac5def541b64
Neeraj-1007 <neerajkumarjha10@gmail.com>
2023-02-26 20:01:56 +0530

    xv c f

--- src/main/java/com/example/FirstSpringBootWithMaven/FirstSpringBootWithMavenApplication.java
+++ src/main/java/com/example/FirstSpringBootWithMaven/FirstSpringBootWithMavenApplication.java
@@ -6,8 +6,35 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FirstSpringBootWithMavenApplication {
 
+/**
+ * Entry method for this appliation
+ */
+
 	public static void main(String[] args) {
+
 		SpringApplication.run(FirstSpringBootWithMavenApplication.class, args);
+
+
 	}
+
+	public static String add(int a,int b){
+	if(a>b) {
+		int c = a + b;
+	}else{
+		int c = a - b;
+	}
+		return c;
+	}
+
+	public static String sub(int a,int b){
+		if(a>b) {
+			int c = a - b;
+		}else{
+			int c = (a-b)*(-1);
+		}
+		return c;
+
+	}
+
 
 }
